1fd931e6fe3970b96dfba0dee07f86097939e718
Foerster, Leroy <leroy.foerster@immonet.de>
2018-09-22 20:31:22 +0200

    reunite files

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,38 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-#include "networking.h"
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+byte mac[] = {
+  0xB0, 0x0B, 0x13, 0x5B, 0x16, 0x05
+};
+
+unsigned int localPort = 8888;      // local port to listen on
+
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet,
+
+EthernetUDP udp;
+
+void initializeUpdConnection(uint8_t first, uint8_t second, uint8_t third, uint8_t last) {
+
+  Ethernet.begin(
+    mac,
+// An EthernetUDP instance to let us send and receive packets over UDP
+    IPAddress(first, second, third, last)
+  );
+  Serial.print("IP: ");
+  Serial.println(Ethernet.localIP());
+  Serial.print("GW: ");
+  Serial.println(Ethernet.gatewayIP());
+  Serial.print("DNS: ");
+  Serial.println(Ethernet.dnsServerIP());
+  Serial.print("Subnet: ");
+  Serial.println(Ethernet.subnetMask());
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  udp.begin(localPort);
+}
 
 void setup() {
   Serial.begin(9600);
