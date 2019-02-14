b420c4b43811ca1d1cef979f94c2f7e6edab9346
Foerster, Leroy <leroy.foerster@immonet.de>
2019-02-14 14:04:55 +0100

    fix example python formatting

--- contrib/clients/python/benchmark.py
+++ contrib/clients/python/benchmark.py
@@ -1,13 +1,13 @@
 import socket
 import time
 
-sock = socket.create_connection(("192.168.58.136", 41))
+sock = socket.create_connection(("192.168.58.131", 41))
 print("Connected to %s:%d" % sock.getpeername())
 
 i = 64
 
 while True:
-        tosend = b'\x02' + i.to_bytes(1, 'little') + (b'\x01\x00\x00' * i)
-        sock.send(tosend)
-        tosend = b'\x02' + i.to_bytes(1, 'little') + (b'\x00\x00\x01' * i)
-        sock.send(tosend)
+    tosend = b'\x02' + i.to_bytes(1, 'little') + (b'\x01\x00\x00' * i)
+    sock.send(tosend)
+    tosend = b'\x02' + i.to_bytes(1, 'little') + (b'\x00\x00\x01' * i)
+    sock.send(tosend)
